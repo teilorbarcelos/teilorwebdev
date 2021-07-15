be2ed594982e126cad56b804f99c1700501c4769
Teilor Souza Barcelos <tsb-@live.com>
2021-07-15 14:10:21 -0300

    typescript and developing mesage added to home

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Teilor Web Developer</title>
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to my portfolio!
+        </h1>
+        <h3>
+        I'm <a href="https://github.com/tsbcylon">Teilor Souza Barcelos</a> and I'm programming this right now!
+          </h3>
+      </main>
+    </div>
+  )
+}
