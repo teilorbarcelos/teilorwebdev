970e260a94eee875e9230e5d5808aa21202480fa
Teilor Souza Barcelos <tsb-@live.com>
2021-07-27 15:11:02 -0300

    header and container styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
+import imgHero from '../public/hero.jpg'
 
 export default function Home() {
   return (
@@ -9,36 +11,61 @@ export default function Home() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300;400;500&display=swap" rel="stylesheet" />
         <title>Teilor Web Developer</title>
       </Head>
 
       {/* HEADER */}
       
       <header className="menu">
-        <div className="menu-left"></div>
-        <div className="menu-logo"></div>
-        <div className="menu-right"></div>
+        <div className="menu-left">
+          <Link href="#">
+            <a className="link">HOME</a>
+          </Link>
+          <Link href="#">
+            <a className="link">ABOUT</a>
+          </Link>
+        </div>
+        <div className="menu-logo">
+        <Link href="/">
+            <a className="link">Teilor.</a>
+          </Link>
+        </div>
+        <div className="menu-right">
+          <Link href="#">
+            <a className="link">WORKS</a>
+          </Link>
+          <Link href="#">
+            <a className="link">GET IN TOUCH</a>
+          </Link>
+        </div>
       </header>
 
       {/* BACKGROUND HERO */}
 
-      <div className="hero">
+      <div className="bg-hero">
 
       </div>
 
       {/* CONTAINER */}
 
-      <section className="container">
-        <div className="container-item">
+      <section className="container hero">
+        <div className="container-item border">
           <h1>Teilor S. Barcelos</h1>
           <h3>Web Developer</h3>
           <h3>Front & Back-end Programming</h3>
           <Link href="#">
-            <a className="link">WORKS</a>
+            <a className="square">WORKS</a>
           </Link>
         </div>
-        <div className="container-item"></div>
+        <div className="container-item border">
+          <div className="img-hero">
+            <Image
+              src={imgHero}
+              alt="Picture of the author"
+            />
+          </div>
+        </div>
       </section>
       
     </div>
