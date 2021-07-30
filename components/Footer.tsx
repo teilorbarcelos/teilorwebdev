415b4b0e2da977134f654943a68ad2fccb7709d8
Teilor Souza Barcelos <tsb-@live.com>
2021-07-30 11:33:06 -0300

    one more detail

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="text-center py-4 in-front">
+        <footer className="text-center py-4">
             <h6 className="mask">Developed by Teilor Souza Barcelos &copy; 2021</h6>
         </footer>
     )
