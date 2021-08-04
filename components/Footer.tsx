5575cd1296cced06f9c43ec369c97c1e0788e91c
Teilor Souza Barcelos <tsb-@live.com>
2021-08-04 12:44:16 -0300

    icons stylization finished

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,12 +5,12 @@ export default function Footer() {
     return (
         <footer className="text-center py-4 in-front">
 
-            <div className="bb-gray2 my-5" id="bb-gray"></div>
+            <div className="bb-gray2 my-5 only-mobile"></div>
 
             {/* <!-- Go to www.addthis.com/dashboard to customize your tools --> */}
-            <div className="addthis_inline_share_toolbox_62v1"></div>
+            <div className="addthis_inline_share_toolbox_62v1 only-mobile"></div>
 
-            <div className="bb-gray2 my-5" id="bb-gray"></div>
+            <div className="bb-gray2 my-5"></div>
 
             <div className="my-5">
                 <Image
